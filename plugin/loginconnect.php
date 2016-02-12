193920961b021740bfbb56e26e1d97db6e9959d6
Vinay Chandil <vinaychandil17@gmail.com>
2016-02-12 19:48:28 +0530

    Changes in login

--- plugin/loginconnect.php
+++ plugin/loginconnect.php
@@ -5,19 +5,23 @@ include 'connection.php';
 $value1=$_POST['rollno'];
 $value2=$_POST['dob'];
 if ($_POST['rollno']) {
-    $_SESSION['rollno'] = $_POST['rollno'];
-    }
-    $_SESSION['rollno'] = $value1;
+	$_SESSION['rollno'] = $_POST['rollno'];
+}
+$_SESSION['rollno'] = $value1;
 $_SESSION['dob'] = $value2;
-$sql="INSERT IGNORE INTO register
-	SET rollno='$value1',dob='$value2'";
-if(!mysql_query($sql))
-	{
-		die("error:" .mysql_error());
-	}	else echo "Submitted successfully";
+$result = mysql_query("SELECT id FROM register WHERE rollno = '$value1' AND dob='$value2'");
+if(mysql_num_rows($result) == 0) 
+{
+	echo 'NOT ELIGIBLE';
+} else 
+{
+	$sql="UPDATE register
+	SET rollno='$value1',dob='$value2' WHERE rollno='$value1' AND dob='$value2'";
+	header('Location:register.php');
+
+}
 
 
-header('Location:register.php');
 
 
 ?>
