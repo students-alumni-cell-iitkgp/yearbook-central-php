c0a8cf5e58dc921ca39b26a8e6c53edd9a2c4200
Hitesh Saini <saini.hitesh4@gmail.com>
2016-03-20 18:53:15 +0530

    Check if session exist

--- plugin/register.php
+++ plugin/register.php
@@ -4,6 +4,11 @@
 include 'connection.php';
 
 	session_start();
+	if (isset($_SESSION['rollno'])) {
+		
+	}else{
+	echo '<script>alert("You need to Log In");window.location.href="login.php";</script>';
+	}
 	$value1=$_SESSION['rollno'];
 	$query = "select * from register where rollno = '$value1'"; 
 	$result = mysql_query($query); 
