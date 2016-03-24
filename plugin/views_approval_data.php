596b9e4c9369fc7e3a00be1a6ea0558a53ae4f09
Hitesh Saini <saini.hitesh4@gmail.com>
2016-03-24 15:18:55 +0530

    Views Approval by User Made

---
+++ plugin/views_approval_data.php
@@ -0,0 +1,14 @@
+<?php
+	require 'connection.php';
+	session_start();
+	if(isset($_POST['id'])){
+		$id= $_POST['id'];
+		$query = "update views set approval = 'approve' where id = '$id'";
+		//$query = "INSERT INTO views (approval) VALUES ('approve') WHERE  deptmate = '$rollno'";
+		if($query_run = mysql_query($query)){
+			echo 1;
+		}else{
+			echo 0;
+		}
+	}	 
+?>
