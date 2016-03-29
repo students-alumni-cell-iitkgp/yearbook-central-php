975fee0bef5b3e54772d4b4f604ff8260cfcdf3e
Vinay Chandil <vinaychandil17@gmail.com>
2016-03-29 17:36:44 +0530

    create

---
+++ plugin/viewsfriend.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include 'connection.php';
+$rollno=$_SESSION['rollno'];
+echo "$rollno";
+$depmate=$_POST['froll'];
+$view=$_POST['viewf'];
+$_SESSION['froll'] = $depmate;
+$_SESSION['viewf'] = $view;
+
+$sql="INSERT INTO views (user,deptmate,views)
+VALUES ('$rollno','$depmate','$view')";
+$success = mysql_query($sql);
+if ($success) 
+ header('Location:department.php');
+else
+	echo mysql_error();
+?>
